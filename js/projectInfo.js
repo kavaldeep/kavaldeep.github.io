42c933d6f674bee61934fc09b2cc458102f07967
kavaldeep <49375690+kavaldeep@users.noreply.github.com>
2021-08-04 18:22:33 +0200

    Kavaldeep

---
+++ js/projectInfo.js
@@ -0,0 +1,39 @@
+const Projects = {
+    Details:{//adding in opposite-chronological order, so it's easy to add later    
+        Template: {
+            ID: "Template",
+            Name: "",
+            SmallDescription: "",
+            LargeDescription: "",
+            DownloadLink: "",
+            EmbedVideoLinkID: "",
+            NoOfProjectImages: 1
+        },
+        MindMapGame:{
+            ID:"MindMapGame",
+            Name: "MindMap",
+            SmallDescription : "Unity3D , NodeJs , MongoDb , C#",
+            LargeDescription: "Memorising is a crucial first step in learning.The faster you memorise, the quicker you learn.The aim of this Game is to help you memorize as quickly as possible. To make it short, the mindmap tries to mimic associative thinking's path.  In a mindmap, we associate a subject with each word, which allows us to quickly learn a topic and recall it if necessary.",
+            NoOfProjectImages: 2,
+  
+        },
+        MindmapWeb: {
+            ID: "MindmapWeb",
+            Name: "MindmapWeb",
+            SmallDescription: "Nodejs , React , MongoDb , HTML , CSS , JQUERY , go.js , BootStrap",
+            LargeDescription: "Memorising is a crucial first step in learning.The faster you memorise, the quicker you learn.The aim of this website is to help you memorize as quickly as possible. To make it short, the mindmap tries to mimic associative thinking's path.  In a mindmap, we associate a subject with each word, which allows us to quickly learn a topic and recall it if necessary.",
+            NoOfProjectImages: 1,
+            DownloadLink: "http://mindmapk.azurewebsites.net/login",
+        },
+        TimeManager: {
+            ID: "TimeManager",
+            Name: "TimeManagerWeb",
+            SmallDescription: "Python , Django , ASP.NET , ChartJS , BootStrap ",
+            LargeDescription: 'Facebook, youtube, and other distractions continuously challenge our brains. The pressure to keep up with these distraction weakens our willpower to do what is necessary, resulting in a lack of time. You can focus on what is essential, keep track of what you have done throughout the day, and remember your goal with this minimalist site.',
+            DownloadLink: "https://testkavaldeep.azurewebsites.net/login/",
+            EmbedVideoLinkID: "",
+            NoOfProjectImages: 4
+        },
+    
+    }
+};
